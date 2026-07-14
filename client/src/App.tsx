4dfba187b8f0df50123b5368ed2095a148171e60
boonshee <42306009+boonshee@users.noreply.github.com>
2026-07-15 03:42:35 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,27 @@
+import {useState} from "react"
+import Login from "./auth/Login"
+import {isLogin} from "./auth/AuthStorage"
 import AppShell from './shell/AppShell'
 
 export default function App() {
-  return <AppShell />
+const [login,setLogin]=useState(
+  isLogin()
+)
+
+
+if(!login){
+
+  return (
+
+    <Login
+
+      onLogin={()=>{
+        setLogin(true)
+      }}
+
+    />
+
+  )
+
+}  return <AppShell />
 }
